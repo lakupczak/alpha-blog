8fed19b40eaac83357522e400521a5bc0cc7685f
Lukasz Kupczak <lakupczak@gmail.com>
2017-09-28 10:59:07 -0400

    cleaned up application

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def index
     @articles = Article.all
@@ -9,7 +10,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   def create
@@ -24,7 +25,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfuly updated"
       redirect_to article_path(@article)
@@ -35,12 +35,10 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
 
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfuly deleted"
     redirect_to articles_path
@@ -49,6 +47,10 @@ class ArticlesController < ApplicationController
 
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
 
