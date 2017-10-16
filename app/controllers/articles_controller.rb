0721b8b414f4f35d2bce14fafbd90b6572040a4e
Lukasz Kupczak <lakupczak@gmail.com>
2017-10-16 13:37:50 -0400

    app created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
   def create
 
     @article = Article.new(article_params)
-    @article.user = User.current_user
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was successfuly created"
       redirect_to article_path(@article)
@@ -57,7 +57,7 @@ class ArticlesController < ApplicationController
 
   def article_params
 
-    params.require(:article).permit(:title, :description)
+  params.require(:article).permit(:title, :description, category_ids: [])
 
   end
 
